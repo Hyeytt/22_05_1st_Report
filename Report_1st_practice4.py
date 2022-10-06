79c1702d16b903968afefd69c6c2fe55225e69a1
Hyeytt <112676923+Hyeytt@users.noreply.github.com>
2022-10-06 13:32:32 +0900

    Add files via upload

---
+++ Report_1st_practice4.py
@@ -0,0 +1,15 @@
+# 연습문제 4
+
+import turtle
+t = turtle .Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t._screen.exitonclick()
