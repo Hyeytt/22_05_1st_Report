4e168516c7d66327fa0d46da8a66afeca920ce83
Hyeytt <112676923+Hyeytt@users.noreply.github.com>
2022-10-07 19:28:57 +0900

    Update Report_1st_practice4.py

--- Report_1st_practice4.py
+++ Report_1st_practice4.py
@@ -1,15 +1,15 @@
 # 연습문제 4
 
-import turtle
-t = turtle .Turtle()
-t.shape("turtle")
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t._screen.exitonclick()
+import turtle                    # turtle 불러오기
+t = turtle .Turtle()             # turtle을 t로 대체하기
+t.shape("turtle")                # 거북이 모양을 사용하겠다는 뜻
+t.forward(100)                   # t를 100만큼 이동시키기
+t.left(90)                       # t를 왼쪽으로 90도 회전시키기
+t.forward(100)                   # t를 100만큼 이동
+t.right(90)                      # t를 오른쪽으로 90도 회전시키기
+t.forward(100)                   # t를 100만큼 이동시키기
+t.right(90)                      # t를 오른쪽으로 90도 회전시키기
+t.forward(100)                   # t를 100만큼 이동시키기
+t.left(90)                       # t를 왼쪽으로 90도 회전시키기
+t.forward(100)                   # t를 100만큼 이동시키기
+t._screen.exitonclick()          # 화면을 클릭해야 종료
